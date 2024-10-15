7226d2a230bae176828a42a42035fb175e82ca9f
Jad Haidar <18439122+jadhaidar@users.noreply.github.com>
2024-10-16 02:47:21 +0400

    add dark mode support in the blog app

--- apps/blog/gatsby-browser.js
+++ apps/blog/gatsby-browser.js
@@ -1,1 +1,12 @@
 import "./src/styles/global.css";
+
+import React from "react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+
+function ThemeProvider({ children, ...props }) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+}
+
+export const wrapRootElement = ({ element }) => {
+  return <ThemeProvider attribute="class">{element}</ThemeProvider>;
+};
--- apps/blog/src/pages/index.tsx
+++ apps/blog/src/pages/index.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import type { HeadProps, PageProps } from "gatsby";
 import {
+  ColorModeSelect,
   InputWithHistory,
   RootList,
   RootListItem,
@@ -27,6 +28,7 @@ function IndexPage(_: PageProps): JSX.Element {
     <div
       className={`grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 sm:p-20`}
     >
+      <ColorModeSelect />
       <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
         <InputWithHistory history="Some previous text" full />
         <StatusSelect />
