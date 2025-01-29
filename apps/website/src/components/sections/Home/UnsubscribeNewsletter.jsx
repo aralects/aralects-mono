9f46e34f754e3034db4b49b8a85bff22df2e0412
alikach97 <ali.kachmar@anghami.com>
2025-01-29 23:48:23 +0400

    add unsubscribe webpage

---
+++ apps/website/src/components/sections/Home/UnsubscribeNewsletter.jsx
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from "react";
+
+// should take subscriberId as a prop
+const UnsubscribeNewsletter = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState(false);
+  const [subscriberId, setSubscriberId] = useState("");
+
+  useEffect(() => {
+    // Retrieve the subscriberId from the URL query string
+    const urlParams = new URLSearchParams(window.location.search);
+    setSubscriberId(urlParams.get("subscriber"));
+  }, []);
+
+  const handleUnsubscribe = async (e) => {
+    e.preventDefault();
+
+    console.log(subscriberId);
+
+    if (!subscriberId) {
+      console.log("i am here 2");
+      setError("Subscriber ID is required");
+      return;
+    }
+
+    setError("");
+    setSuccess(false);
+
+    setIsLoading(true);
+
+    try {
+      const response = await fetch(
+        `https://api.aralects.com/v1/newsletter/subscriber/deactivate`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ subscriber_id: parseInt(subscriberId, 10) }), // Send subscriber_id as an integer
+        },
+      );
+
+      console.log("i am here ");
+      console.log(response);
+
+      if (!response.ok) {
+        const error = await response.json();
+        throw new Error("Failed to unsubscribe");
+      }
+
+      setSuccess(true);
+    } catch (err) {
+      console.log(err);
+      setError("Something went wrong. Please try again later.");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Unsubscribe Newsletter</h1>
+    </div>
+  );
+};
+
+export default UnsubscribeNewsletter;
