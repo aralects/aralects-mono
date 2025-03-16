d28565b707345597003279bff4b4f38ce001cfa9
alikach97 <ali.kachmar@anghami.com>
2025-03-16 15:51:33 +0400

    fix importing env from root env

--- apps/demo/vite.config.ts
+++ apps/demo/vite.config.ts
@@ -1,25 +1,33 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: "dist",
-    rollupOptions: {
-      input: {
-        main: "./index.html",
+export default defineConfig(({ mode }) => {
+  // Load environment variables from monorepo root
+  process.env = {
+    ...process.env,
+    ...loadEnv(mode, path.resolve(__dirname, "../../")),
+  };
+
+  return {
+    plugins: [react()],
+    build: {
+      outDir: "dist",
+      rollupOptions: {
+        input: {
+          main: "./index.html",
+        },
       },
     },
-  },
-  publicDir: "public", // Ensure Vite serves files correctly
-  server: {
-    host: "0.0.0.0", // This allows access from your phone
-    port: 4322,
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    publicDir: "public", // Ensure Vite serves files correctly
+    server: {
+      host: "0.0.0.0", // This allows access from your phone
+      port: 4322,
     },
-  },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  };
 });
