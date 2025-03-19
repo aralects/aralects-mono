c38c62cf14c906230b91ad49c880f8fa55e74a3f
Christian Cayralat <christian-cayralat@hotmail.com>
2025-03-19 12:45:41 +0400

    Copywriting contact us and demo

--- apps/website/src/constants/contact.ts
+++ apps/website/src/constants/contact.ts
@@ -1,17 +1,18 @@
 export const CONTACT_CONTENT = {
-  TITLE: 'Get In Touch',
-  DESCRIPTION: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dig.',
+  TITLE: "Get In Touch",
+  DESCRIPTION:
+    "Got questions, ideas, or just want to say hi? We're always eager to connect! Reach out through any of the channels below, and we'll get back to you in no time.",
 } as const;
 
 export const CONTACT_COLORS = {
-  TITLE: '#3D2B66',
-  BACKGROUND: '#8262B0',
+  TITLE: "#3D2B66",
+  BACKGROUND: "#8262B0",
 } as const;
 
 export const CONTACT_LAYOUT = {
   CONTAINER_PADDING: {
-    DEFAULT: 'p-8',
-    MD: 'md:p-16 md:pl-24',
-    XL: 'xl:px-24 xl:py-12',
+    DEFAULT: "p-8",
+    MD: "md:p-16 md:pl-24",
+    XL: "xl:px-24 xl:py-12",
   },
-} as const; 
+} as const;
