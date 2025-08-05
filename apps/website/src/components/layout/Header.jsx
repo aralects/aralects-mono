ce726d390fdd2ae55ad1d4fac83ef0c7dd94f72c
Christian Cayralat <christian-cayralat@hotmail.com>
2025-08-05 10:17:14 +0300

    Change images of team members and update hid the demo page in menu bar

--- apps/website/src/components/layout/Header.jsx
+++ apps/website/src/components/layout/Header.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 
 const links = [
   { href: "/", label: "Home" },
-  { href: "/demo", label: "Demo" },
+  // { href: "/demo", label: "Demo" },
   { href: "/about", label: "About" },
   { href: "https://blog.aralects.com", label: "Blog" },
   { href: "/contact", label: "Contact Us" },
