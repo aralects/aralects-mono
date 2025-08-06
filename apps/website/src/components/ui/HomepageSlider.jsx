c67bb9cf8fa37931750b7129af5d2c41db6e1ec7
marceid <73819419+marceid@users.noreply.github.com>
2025-08-06 09:47:48 +0300

    Homepage updates

---
+++ apps/website/src/components/ui/HomepageSlider.jsx
@@ -0,0 +1,126 @@
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import { useEffect, useState } from "react";
+
+function HomepageSlider({ isFirstPromo = false }) {
+  const [isClient, setIsClient] = useState(false);
+
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: false,
+    customPaging: (i) => (
+      <div className={"dots-bg h-[9px] w-[9px] rounded-full"}></div>
+    ),
+  };
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  // Only render the slider on the client side
+  if (!isClient) {
+    return <div>Loading slider...</div>;
+  }
+
+  return (
+    <div className="slider-container mt-[20px]">
+      {isFirstPromo ? (
+        <Slider {...settings}>
+          <div className="pb-[30px] pr-[10px]">
+            <img
+              src="/img/homepage-promo-1-prompt.png"
+              alt=""
+              className="h-[56px] max-w-[150px] translate-x-[-6px] object-contain"
+            />
+            <div className="font-LivvicMedium text-glow-xl text-md intersect-once motion-safe:intersect:animate-fade-in-up mt-1 font-bold transition motion-safe:opacity-0 md:text-lg">
+              AI-Powered Customization
+            </div>
+            <p className="font-SpaceGroteskLight md:text-md intersect-once motion-safe:intersect:animate-fade-in-up mt-1 font-thin text-white transition motion-safe:opacity-0">
+              Move at your own speed with customizable lessons.
+            </p>
+          </div>
+          <div className="pb-[30px]">
+            <img
+              src="/img/homepage-promo-1-script.png"
+              alt=""
+              className="h-[56px] max-w-[100px]  translate-x-[-6px] object-contain"
+            />
+            <div className="font-LivvicMedium text-glow-xl text-md intersect-once motion-safe:intersect:animate-fade-in-up mt-1 font-bold transition motion-safe:opacity-0 md:text-lg">
+              Script Settings
+            </div>
+            <p className="font-SpaceGroteskLight md:text-md intersect-once motion-safe:intersect:animate-fade-in-up mt-1 font-thin text-white transition motion-safe:opacity-0">
+              Toggle between Arabic script, Latin transliteration, or a combined
+              view so you learn in the format that works best for you.
+            </p>
+          </div>
+          <div className="pb-[30px] pl-[10px]">
+            <img
+              src="/img/homepage-promo-1-dialects.png"
+              alt=""
+              className="h-[56px] max-w-[190px]  translate-x-[-6px] object-contain"
+            />
+            <div className="font-LivvicMedium text-glow-xl text-md intersect-once motion-safe:intersect:animate-fade-in-up mt-1 font-bold transition motion-safe:opacity-0 md:text-lg">
+              Main & Secondary Dialect
+            </div>
+            <p className="font-SpaceGroteskLight md:text-md intersect-once motion-safe:intersect:animate-fade-in-up mt-1 font-thin text-white transition motion-safe:opacity-0">
+              Choose a primary and secondary dialect, every lesson dynamically
+              shows vocabulary and phrases side‑by‑side.
+            </p>
+          </div>
+        </Slider>
+      ) : (
+        <Slider {...settings}>
+          <div className="pb-[30px] pr-[10px]">
+            <img
+              src="/img/homepage-promo-2-audio.png"
+              alt=""
+              className="h-[56px] max-w-[60px] translate-x-[-6px] object-contain"
+            />
+            <div className="font-LivvicMedium text-glow-xl text-md intersect-once motion-safe:intersect:animate-fade-in-up mt-1 font-bold transition motion-safe:opacity-0 md:text-lg">
+              Audio Pronunciation
+            </div>
+            <p className="font-SpaceGroteskLight md:text-md intersect-once motion-safe:intersect:animate-fade-in-up mt-1 font-thin text-white transition motion-safe:opacity-0">
+              Hear every word and phrase spoken by native speakers.
+            </p>
+          </div>
+          <div className="pb-[30px]">
+            <img
+              src="/img/homepage-promo-2-explain.png"
+              alt=""
+              className="h-[56px] max-w-[100px]  translate-x-[-6px] object-contain"
+            />
+            <div className="font-LivvicMedium text-glow-xl text-md intersect-once motion-safe:intersect:animate-fade-in-up mt-1 font-bold transition motion-safe:opacity-0 md:text-lg">
+              “Explain Answer” Feature
+            </div>
+            <p className="font-SpaceGroteskLight md:text-md intersect-once motion-safe:intersect:animate-fade-in-up mt-1 font-thin text-white transition motion-safe:opacity-0">
+              Tappable chat icon that opens the AI bot for an in‑depth
+              breakdown, great for curious students who want more nuance.
+            </p>
+          </div>
+          <div className="pb-[30px] pl-[10px]">
+            <img
+              src="/img/homepage-promo-2-dialect.png"
+              alt=""
+              className="h-[56px] max-w-[190px]  translate-x-[-6px] object-contain"
+            />
+            <div className="font-LivvicMedium text-glow-xl text-md intersect-once motion-safe:intersect:animate-fade-in-up mt-1 font-bold transition motion-safe:opacity-0 md:text-lg">
+              Secondary Dialect
+            </div>
+            <p className="font-SpaceGroteskLight md:text-md intersect-once motion-safe:intersect:animate-fade-in-up mt-1 font-thin text-white transition motion-safe:opacity-0">
+              Every lesson shows the word being learned in the secondary dialect
+              chosen by you.
+            </p>
+          </div>
+        </Slider>
+      )}
+    </div>
+  );
+}
+
+export default HomepageSlider;
